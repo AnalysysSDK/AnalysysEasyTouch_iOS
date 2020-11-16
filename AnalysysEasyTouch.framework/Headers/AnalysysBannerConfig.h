93b9b950e8f4e3fde6492aa5e930d2d55a6ccc59
shadeless <shadeless@126.com>
2020-11-16 11:58:22 +0800

    v1.2.4

--- AnalysysEasyTouch.framework/Headers/AnalysysBannerConfig.h
+++ AnalysysEasyTouch.framework/Headers/AnalysysBannerConfig.h
@@ -23,6 +23,9 @@ NS_ASSUME_NONNULL_BEGIN
 /// 装载 banner 的容器，非必填，若调用 AnalysysEaManager loadBanner 接口返回 cell 行高，该值可不必传
 @property (nonatomic, weak) UIView *container;
 
+/// 当事件未触发或活动已结束时，将不展示 banner，非必填，默认 NO
+@property (nonatomic, assign) BOOL hidesWhenNotAvailable;
+
 @end
 
 NS_ASSUME_NONNULL_END
